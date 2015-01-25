41507f34d6026dcf6916cdf6a3a896335de8a0e3
StreamLiner <jimmy.xrail@me.com>
2015-01-25 23:17:51 +0800

    算法小修小补
    增加了一些错误的handling，catalog数据每页显示120个增加速度

--- neiman_marcus/spiders/neiman_spider.py
+++ neiman_marcus/spiders/neiman_spider.py
@@ -1,4 +1,5 @@
 import scrapy
+import time
 from neiman_marcus.items import NeimanMarcusItem
 from scrapy.http import Request
 from urlparse import urljoin
@@ -19,6 +20,7 @@ class NeimanSpider(scrapy.Spider):
 	# start_urls = ["http://www.neimanmarcus.com/Womens-Clothing/Categories/cat17740747_cat000001_cat000000/c.cat"]
 	
 	def __init__(self):
+		self.start_time = time.time()
 		self.driver = webdriver.Chrome("./chromedriver")
 		self.driver.implicitly_wait(30)
 		category_urls = []
@@ -26,25 +28,59 @@ class NeimanSpider(scrapy.Spider):
 		
 		# append all items URLs before parsing
 		self.driver.get(self.start_urls[0]) #start from the first page
-		time.sleep(15)
+		time.sleep(5)
+		# form_closed = 0;
+		# while form_closed == 0:
+		# 	try:
+		# 		close_button = self.driver.find_element_by_id("closeButton")
+		# 		close_button.click()
+		# 	except Exception, e:
+		# 		print("Button does not exist!")
+		# 		time.sleep(1)
+		# 	else: form_closed = 1
+		self.driver.implicitly_wait(10)
 		close_button = self.driver.find_element_by_id("closeButton")
 		close_button.click()
-		
+		time.sleep(1)
+		view120 = self.driver.find_element_by_xpath("//dd[@id='HundredTwentyPerPage']/a");
+		view120.click()
 		page_source = self.driver.page_source
 		urls = self.get_item_urls(page_source)
 		self.start_urls.extend(urls)
-
+		#next_page = None
+		#while next_page == None:
+		time.sleep(5)
 		next_page = self.driver.find_element_by_xpath("//a[@id='paging_next']")
+
 		total_pages = int(self.driver.find_element_by_xpath("//*[@id='epagingBottom']/li[7]").get_attribute("pagenum"))
 		print total_pages
 		# next_url = next_page.get_attribute("href")
-		for page in xrange(2):
+		for page in xrange(total_pages-1):
+		# for page in xrange(1):
 			print page
+			time.sleep(3)
+			next_clicked = 0
+			try:
+				next_page.click()
+			except Exception, e:
+				time.sleep(1)
+			self.driver.implicitly_wait(3)
 			time.sleep(5)
-			next_page.click()
-			time.sleep(2)
 			self.start_urls.extend(self.get_item_urls(self.driver.page_source))
 			next_page = self.driver.find_element_by_xpath("//a[@id='paging_next']")
+			# while next_clicked == 0:
+			# 	try:
+			# 		next_page.click()
+			# 	except Exception, e:
+			# 		time.sleep(1)
+			# 		print "try"
+			# 	else:
+			# 		next_clicked = 1
+			# self.start_urls.extend(self.get_item_urls(self.driver.page_source))
+			# next_page =  None
+			# if page < total_pages-1:
+			# 	while next_page == None:
+			# 		next_page = self.driver.find_element_by_xpath("//a[@id='paging_next']")
 	
 	def __del__(self):
 		self.driver.close()
@@ -59,6 +95,7 @@ class NeimanSpider(scrapy.Spider):
 
 	def parse(self, response):
 		yield self.parse_item(response)
+		print("--- Time elapsed: %s Seconds---" % (time.time() - self.start_time))
 
 	def parse_item(self, response):
 		item=NeimanMarcusItem()
